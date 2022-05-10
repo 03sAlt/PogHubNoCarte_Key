1affa2d583a0ce85b143b24dcb92bec9351965c9
03sAlt <102461727+03sAlt@users.noreply.github.com>
2022-05-09 20:14:34 -1200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,3 @@
---- Discord invite
-loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/AutoinviteDiscord03s/main/README.md"))()
-
-
 ----- MAP ------
 --[[
 [+] Online Business Simulator 2
@@ -23,20 +19,22 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/AutoinviteDis
 [+] Kick Off
 ]]
 
---- Set Gui
-loadstring(game:HttpGet("https://pastebin.com/raw/Cs4Mxamd"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/asdsadsadd/main/README.md"))()
 
    local Pl = game.PlaceId
     if Pl == 5327880096 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/OnlineBusinessSimulator2/main/README.md"))()
     end
+    
     if Pl == 8781849572 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/StarSimulatorBeta/main/README.md"))()
     end
+    
     if Pl == 6904735821 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/ShoeSimulator/main/README.md"))()   
     end
-    if Pl == 6953291455 or Pl == 6953291455  then
+    
+    if Pl == 6953291455 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/EatingSimulator/main/README.md"))()
     end
 
@@ -51,18 +49,23 @@ loadstring(game:HttpGet("https://pastebin.com/raw/Cs4Mxamd"))()
     if Pl == 5902977746 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/03sAlt/UltimateTowerDefense/main/README.md"))()
     end 
+    
     if Pl == 8884433153 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/CollectAllPets/main/README.md"))()
     end 
+    
     if Pl == 4003872968 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/GiantSurvival/main/README.md"))()
     end 
+    
     if Pl == 189707 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/NaturalDisasterSurvival/main/README.md"))()
     end 
+    
     if Pl == 3101667897 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/LegendsOfSpeedOP/main/README.md"))()
     end 
+    
     if Pl == 5326405001 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/BaseBattles/main/README.md"))()
     end
